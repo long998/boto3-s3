292643a659dac005c40159d3cfdbb56c4acf2a2f
BILLZHANG <zhang21cn@hotmail.com>
2020-10-22 10:33:51 +1100

    commit s3file second

--- s3file.py
+++ s3file.py
@@ -4,3 +4,18 @@ import boto3
 s3 = boto3.resource('s3')
 s3.Bucket('mybucket').download_file('hello.txt', '/tmp/hello.txt')
 
+#download object, using open file as object
+bucket = s3.Bucket('meykey')
+with open('filename', 'wb') as data:
+    bucket.download_fileobj('mykey', data)
+
+#clear using bucket to access file. but you can use client or resource.
+s3 = boto3.resource('s3')
+s3.meta.client.download_file('mybucket', 'hello.txt', '/tmp/hello.txt')
+
+#upload
+data = b'hello,it is add info'
+bucket.upload_fileobj(data, 'mykey')
+
+#same, you can use resource or client
+s3.upload_fileobj(data, 'mybucket', 'mykey')
